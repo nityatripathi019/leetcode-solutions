06ccb60a4bc6241a6881719386921d8f8541c8e5
NITYA TRIPATHI <154739165+nityatripathi019@users.noreply.github.com>
2026-08-12 20:26:43 +0530

    Time: 3 ms (86.05%), Space: 43.4 MB (41.34%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValid(String str) {
+      Stack<Character> s = new Stack <>();
+      for(int i=0;i<str.length();i++){
+        char ch = str.charAt(i);
+        //opening
+        if(ch=='('|| ch=='{' || ch=='['){
+            s.push(ch);
+        }else{
+            if(s.isEmpty()){
+                return false;
+            }
+            if((s.peek()=='(' && ch==')')||(s.peek()=='{' && ch=='}')||(s.peek()=='[' && ch==']')){
+                s.pop();
+            }else{
+                return false;
+            }
+        }
+      }  
+      if(s.isEmpty()){
+        return true;
+      }else{
+        return false;
+      }
+    }
+}
